f498f965e0648ed922ee260bf9f9815ec02438f2
ohs2033 <glnt1@naver.com>
2015-07-30 18:10:27 +0900

    Create Firstfactorial.rb

---
+++ Firstfactorial.rb
@@ -0,0 +1,22 @@
+
+
+def FirstFactorial(num)
+  i=1
+  k=1
+  while(i<=num) do
+  k=k*i
+  
+  i= i+1
+    
+ 
+	end
+  
+  
+  num=k
+  return num 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+FirstFactorial(STDIN.gets) 
